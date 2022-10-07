35abcab14e64df6f452d617c3bdecbfb27179073
Ritesh Zode <riteshzode1122@gmail.com>
2022-10-07 11:56:22 +0530

    news extract selenium -- project completed

---
+++ news-extract-data.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+#############################################
+
+#############################################
+# Step 1
+website_link = "https://www.thesun.co.uk/sport/football/"
+
+chrome_driver_path = "C:\Development\chromedriver.exe"
+#############################################
+
+#############################################
+# Step 2
+
+# this options class will scrape the website  by opening it in background
+# -----------------------
+options = Options()
+options.headless = True
+# -----------------------
+
+service = Service(chrome_driver_path)
+
+driver = webdriver.Chrome(service=service, options=options)  # options=options (extra)
+
+driver.get(website_link)
+
+#############################################
+
+#############################################
+# Step 3
+
+containers = driver.find_elements(By.CLASS_NAME, 'teaser__copy-container')
+
+titles = []
+subtitles = []
+links = []
+
+for i in containers:
+    title = i.find_element(By.CLASS_NAME, 'teaser__headline').text
+    subtitle = i.find_element(By.CLASS_NAME, 'teaser__subdeck').text
+    link = i.find_element(By.CLASS_NAME, 'text-anchor-wrap').get_attribute("href")
+
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+
+#############################################
+
+#############################################
+# Step 4
+
+
+my_dict = {"title": titles, "subtitle": subtitles, "link": links}
+
+df = pd.DataFrame(my_dict)
+
+df.to_csv("file.csv")
+
+#############################################
+# Step 5
+
+driver.quit()
+
+#############################################
